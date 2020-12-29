49575ebe3dd6c557990dd25a444b2a2d81b06abc
Gurar <jozyk@inbox.lt>
2020-12-29 14:05:06 +0200

    kintamasi menesiai

---
+++ js/components/menesiai.js
@@ -0,0 +1,16 @@
+const meniesiai = [
+    'Sausis', 
+    'Vasaris', 
+    'Kovas', 
+    'Balandis', 
+    'Geguze', 
+    'Birzelis', 
+    'Liepa', 
+    'Rugpjutis', 
+    'Rugsejis', 
+    'Spalis', 
+    'Lapkritis', 
+    'Gruodis'
+];
+
+export {meniesiai};
